dd597d0b0217e087eb524125624efa0315fd3c90
sganas360 <sganas@caesars.com>
2023-03-22 17:55:08 -0400

    Initial Commit

---
+++ BusinessCard/BusinessCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusinessCardApp.swift
+//  BusinessCard
+//
+//  Created by Shun Ganas on 3/22/23.
+//
+
+import SwiftUI
+
+@main
+struct BusinessCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
